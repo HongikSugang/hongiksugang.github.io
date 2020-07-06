689f5aa0bfb6f84151d445449918563da5e5cf63
HongikSugang <hongiksugang@gmail.com>
2020-07-06 23:40:17 +0900

    Randomize the number of subjects

--- cssjs/click.js
+++ cssjs/click.js
@@ -3,20 +3,21 @@ const fucked = document.getElementById('fucked');
 const sugangButton = document.getElementById('sugangButton');
 let time = new Date;
 const timeBeforeClick = time.getTime();
+const subject_number = Math.floor(Math.random() * 5 + 3)
 
 
 if (failed) {
   for (let i = 0; i < failed.length; i++) {
     failed.item(i).addEventListener('click', function() {
-      alert('망했습니다! \n\"수강신청\" 버튼을 클릭해야 합니다!');
-      location.replace("./sugang");
+      alert('"수강신청" 버튼을 클릭해야 합니다!');
+      location.replace("./main");
     });
   }
 }
 if (fucked) {
   fucked.addEventListener('click', function() {
-    window.alert('ㄹㅇ 망했습니다.\n\"수강신청\" 버튼을 클릭해야 합니다!!!');
-    location.replace('./sugang');
+    window.alert('"수강신청" 버튼을 클릭해야 합니다!!!');
+    location.replace('./main');
   });
 }
 if (sugangButton) {
@@ -26,4 +27,11 @@ if (sugangButton) {
     alert(timeRecorded/1000 + '초 걸렸습니다');
     history.back()
   });
+}
+console.log(subject_number)
+for (let i = subject_number; i <= 7; i++) {
+  obj = document.getElementsByClassName(i)
+  for (let j = 1; j >= 0; j--) {
+    obj[j].classList.add("display-none")
+  }
 }
